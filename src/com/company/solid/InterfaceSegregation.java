71c29f1823a6d8ad5dc16ea35b318aa7bb81464a
Giang <giangnguyen010801@gmail.com>
2022-03-08 09:20:46 +0700

    Lần 1

---
+++ src/com/company/solid/InterfaceSegregation.java
@@ -0,0 +1,67 @@
+package com.company.solid;
+
+class Document{
+
+}
+interface IMachine{
+    void Print(Document d);
+    void Scan(Document d);
+    void Fax(Document d);
+}
+
+interface IPrinter
+{
+    void Print(Document d);
+}
+interface IFaxer
+{
+    void Fax(Document d);
+}
+interface IScanner
+{
+    void Scan(Document d);
+}
+interface IMultiFunction extends IFaxer,IPrinter,IScanner{
+
+}
+class MultiFuncPrinter implements IMultiFunction {
+
+    @Override
+    public void Print(Document d) {
+        System.out.print("Print");
+    }
+
+    @Override
+    public void Fax(Document d) {
+        System.out.print("Fax");
+    }
+
+    @Override
+    public void Scan(Document d) {
+        System.out.print("Scan");
+    }
+}
+
+class OldPrinter implements IPrinter {
+    @Override
+    public void Print(Document d) {
+
+    }
+}
+class MidPrinter implements  IPrinter,IScanner{
+
+    @Override
+    public void Print(Document d) {
+
+    }
+
+    @Override
+    public void Scan(Document d) {
+
+    }
+}
+public class InterfaceSegregation {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+}
---
+++ src/com/company/solid/Liskov.java
@@ -0,0 +1,53 @@
+package com.company.solid;
+
+class Rectangle{
+    int Width;
+    int Height;
+    public void setWidth(int width) {
+        this.Width = width;
+    }
+
+    public void setHeight(int height) {
+        this.Height = height;
+    }
+    public Rectangle(int w, int h)
+    {
+        Width = w;
+        Height = h;
+    }
+    public Rectangle(){
+        Width=0;
+        Height=0;
+    }
+    @Override
+    public String toString(){
+        return "Width: "+Width +" "+"Height: "+ Height;
+    }
+}
+class Square extends Rectangle{
+    @Override
+    public void setWidth(int width) {
+        super.setWidth(width);
+        super.setHeight(width);
+    }
+    @Override
+    public void setHeight(int height) {
+        super.setWidth(height);
+        super.setHeight(height);
+    }
+}
+public class Liskov {
+    static int Area(Rectangle r){
+        return r.Height*r.Width;
+    }
+
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle(2, 3);
+        System.out.println(r);
+        System.out.println(Area(r));
+        Rectangle s1 = new Square();
+        s1.Width=2;
+        System.out.println(s1);
+        System.out.println(Area(s1));
+    }
+}
